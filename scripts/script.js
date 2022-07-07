fa5337c2082028bf1e12d236687626ea6af94eae
Benja <benjaminokumu12@gmail.com>
2022-07-07 13:58:50 +0300

    jsprof

--- scripts/script.js
+++ scripts/script.js
@@ -5,4 +5,29 @@ const myCompany = document.querySelector('h2');
 myCompany.textContent = 'Teknohub';
 
 let Name = "Benjamin";
-Name.textContent
+
+// let lastName = 'Benjamin';
+// if(lastName === 'Benjamin'){
+//     alert('yay, Thats my last name');
+// } else {
+//     alert('awww, my last name is Okumu');
+// }
+
+
+function multiply(num1, num2) {
+    let result = num1 * num2;
+    return result;
+}
+console.log(multiply(4, 7));
+
+
+// document.querySelector('html').addEventListener('click',
+// funsction(), {
+//     alert('ouch! Stop poking me');
+// }
+// );
+
+let myHTML = document.querySelector('html');
+myHTML.addEventListener('click', function() {
+    alert('Ouch! Stop poking me!');
+});
