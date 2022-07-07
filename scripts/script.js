7203121283b5c6e51b029ad5b296113a01ee94ce
Benja <benjaminokumu12@gmail.com>
2022-07-07 14:18:48 +0300

    jsprof

--- scripts/script.js
+++ scripts/script.js
@@ -27,7 +27,24 @@ console.log(multiply(4, 7));
 // }
 // );
 
-let myHTML = document.querySelector('html');
-myHTML.addEventListener('click', function() {
-    alert('Ouch! Stop poking me!');
-});
+// let myHTML = document.querySelector('html');
+// myHTML.addEventListener('click', function() {
+//     alert('Ouch! Stop poking me!');
+// });  // Anonymous functions
+
+// document.querySelector('html').addEventListener('click', () => {
+//     alert('Ouch! Stop poking me!');
+// });   // Anonymous functions
+
+////Adding image changer
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/img1.png') {
+        myImage.setAttribute('src', 'images/img2.png');
+    } else {
+        myImage.setAttribute('src', 'images/img1.png')
+    }
+}
