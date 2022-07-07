e94bafb56073b8a648606b9d6cb85cd15907a859
Benja <benjaminokumu12@gmail.com>
2022-07-07 13:15:56 +0300

    jsdev

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+const myPosition = document.querySelector('h1');
+myPosition.textContent = 'Software Engineer';
+
+const myCompany = document.querySelector('h2');
+myCompany.textContent = 'Teknohub';
+
+let Name = "Benjamin";
+Name.textContent
